1ef29c2894568bbe577bb3330c138765f6a53802
Tim4Lu <artem.lubavin@gmail.com>
2026-08-15 12:20:23 +0300

    Reset to working commit 5095731 and bump version to v133

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.newsapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 130
-        versionName = "1.0.130"
+        versionCode = 133
+        versionName = "1.0.133"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
