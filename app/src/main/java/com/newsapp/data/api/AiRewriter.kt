8abc56dbd5b4a09a2e88bc54c30b768f6936c40a
Tim4Lu <artem.lubavin@gmail.com>
2026-08-15 20:54:26 +0300

    fix: add missing Ktor header import in AiRewriter for v159

--- app/src/main/java/com/newsapp/data/api/AiRewriter.kt
+++ app/src/main/java/com/newsapp/data/api/AiRewriter.kt
@@ -9,6 +9,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.engine.cio.endpoint
 import io.ktor.client.request.post
+import io.ktor.client.request.header
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
