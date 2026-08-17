96dc2a31dabcf0943d6bb7619ab6bec5538a048a
Tim4Lu <artem.lubavin@gmail.com>
2026-08-17 11:01:28 +0300

    feat: make source link clickable in telegram caption v203

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.newsapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 199
-        versionName = "1.0.199"
+        versionCode = 203
+        versionName = "1.0.203"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
