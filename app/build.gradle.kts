813f9ed6c29bc7a935ade1073b8b51c663b66456
Tim4Lu <artem.lubavin@gmail.com>
2026-08-19 15:17:01 +0300

    fix: sync versionCode and versionName to 238 to fix update loop

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 234
-        versionName = "1.0.237"
+        versionName = "1.0.238"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
