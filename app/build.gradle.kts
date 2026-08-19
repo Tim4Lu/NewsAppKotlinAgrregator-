94abec73387ea394dfb57f4fffe29b9498346b5b
Tim4Lu <artem.lubavin@gmail.com>
2026-08-19 12:28:15 +0300

    fix: decode &#8217; and &rsquo; HTML entities to apostrophes in RSS parser v226

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.newsapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 225
-        versionName = "1.0.225"
+        versionCode = 226
+        versionName = "1.0.226"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
