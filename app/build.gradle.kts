fc42fe884d239b33e18ab5bbe3ae81cfc11510d7
Tim4Lu <artem.lubavin@gmail.com>
2026-08-26 19:09:30 +0300

    chore: force new workflow execution with version bump to 287

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.newsapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 285
-        versionName = "1.0.285"
+        versionCode = 287
+        versionName = "1.0.287"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
