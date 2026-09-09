d05d13d1e14e49a4604139261bda1d789dacb47f
Tim4Lu <artem.lubavin@gmail.com>
2026-09-09 19:16:11 +0300

    fix: піднято версію до 314

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.newsapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 313
-        versionName = "1.0.313"
+        versionCode = 314
+        versionName = "1.0.314"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
