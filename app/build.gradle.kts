72e0980f49c837a640f398cced4cf2a208c1c5c9
Tim4Lu <artem.lubavin@gmail.com>
2026-09-24 13:44:22 +0300

    fix: bump to 363

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.newsapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 362
-        versionName = "1.0.362"
+        versionCode = 363
+        versionName = "1.0.363"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
